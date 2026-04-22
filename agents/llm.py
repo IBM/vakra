f2f50db3ab55e6025cd5d9ded2ab25b787c2e82b
Ankita Naik <ankita.naik@ibm.com>
2026-04-22 11:11:16 -0400

    Add open source models.

--- agents/llm.py
+++ agents/llm.py
@@ -23,10 +23,16 @@ class RITSChatModel(BaseChatModel):
 
     # Mapping from endpoint name (short) to payload model name (full)
     MODEL_NAME_MAPPING: Dict[str, str] = {
-        "llama-3-3-70b-instruct": "meta-llama/llama-3-3-70b-instruct",
-        "gpt-oss-120b": "openai/gpt-oss-120b",
+        # Open Source Models
         "qwen3-5-397b-a17b-fp8": "qwen/qwen3.5-397B-A17B-FP8", 
-        "mistral-large-3-675b-2512-fp4": "mistralai/Mistral-Large-3-675B-Instruct-2512-NVFP4"
+        "mistral-large-3-675b-2512-fp4": "mistralai/Mistral-Large-3-675B-Instruct-2512-NVFP4",
+        "glm-5-1": "",
+        "kimi-k2-5":"moonshotai/Kimi-K2.5",
+        "gpt-oss-120b": "openai/gpt-oss-120b",
+        # smaller models
+        "llama-3-3-70b-instruct": "meta-llama/llama-3-3-70b-instruct",
+        "qwen2-5-72b-instruct": "Qwen/Qwen2.5-72B-Instruct",
+
     }
 
     model_name: str
